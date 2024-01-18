d21c15ce66083408fff84a35e177cbaa843e719a
HW <khaleddortmound123@gmail.com>
2024-01-18 14:50:35 +0200

    App text styles are finished

---
+++ lib/core/utilis/app_text_styles.dart
@@ -0,0 +1,48 @@
+
+
+
+import 'package:flutter/material.dart';
+import 'package:chef_app/core/utilis/app_colors.dart';
+
+class AppTextStyles
+{
+
+  static const TextStyle font16 = TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w700,
+      fontFamily: 'lato',
+      color: AppColors.grey);
+
+  static const TextStyle font24 = TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.w700,
+      fontFamily: 'poppins',
+      color: AppColors.white);
+
+  static const TextStyle font17 = TextStyle(
+      fontSize: 17,
+      fontWeight: FontWeight.w600,
+      fontFamily: 'urbanist',
+      color: AppColors.black);
+
+  static const TextStyle font14 = TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+      fontFamily: 'lato',
+      color: AppColors.white);
+  static const TextStyle font32 = TextStyle(
+      fontSize: 32,
+      fontWeight: FontWeight.w700,
+      fontFamily: 'lato',
+      color: AppColors.black);
+  static const TextStyle font36 = TextStyle(
+      fontSize: 36,
+      fontWeight: FontWeight.w700,
+      fontFamily: 'lato',
+      color: AppColors.black);
+  static const TextStyle font12 = TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.w700,
+      fontFamily: 'poppins',
+      color: AppColors.grey);
+}

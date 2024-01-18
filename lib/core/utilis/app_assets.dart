dcf380a6755811930126cd3a99ee3a17ba7943e5
HW <khaleddortmound123@gmail.com>
2024-01-18 13:32:21 +0200

    App assets and colors files are finished

---
+++ lib/core/utilis/app_assets.dart
@@ -0,0 +1,12 @@
+
+
+class AppAssets
+{
+  static const String basePath='assets/images/';
+  static const String logo='${basePath}logo.png';
+  static const String logoSvg='${basePath}logo_svg.png';
+  static const String lock='${basePath}lock.png';
+  static const String user='${basePath}user.png';
+
+
+}
